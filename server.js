d55e28c3c3613c052c9c84c71deaf1645ed98846
sarangraii <sarang2452@gmail.com>
2026-01-07 11:32:21 +0530

    change in cors

--- server.js
+++ server.js
@@ -10,10 +10,20 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-  credentials: true
-}));
+// app.use(cors({
+//   origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+//   credentials: true
+// }));
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3000',
+      'https://green-cart-logistics-frontend-mu.vercel.app'
+    ],
+    credentials: true
+  })
+);
+
 
 // MongoDB Connection
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/greencart', {
